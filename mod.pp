c039b31f88933594c302e5b039809594f241f343
williamfp4 <william_franz2002@hotmail.com>
2024-06-21 15:57:44 -0300

    Updating the Security Benchmark + Removing unnecessary mods from mod.pp

--- mod.pp
+++ mod.pp
@@ -1,16 +1,7 @@
 mod "local" {
   title = "powerpipe"
   require {
-    mod "github.com/turbot/steampipe-mod-aws-insights" {
-      version = "*"
-    }
     mod "github.com/turbot/steampipe-mod-aws-thrifty" {
-      version = "*"
-    }
-    mod "github.com/turbot/steampipe-mod-aws-tags" {
-      version = "*"
-    }
-    mod "github.com/turbot/steampipe-mod-aws-top-10" {
       version = "*"
     }
     mod "github.com/turbot/steampipe-mod-aws-perimeter" {
--- security.pp
+++ security.pp
@@ -2,10 +2,30 @@
 benchmark "aws_perimeter_security" {
   title         = "Security"
   children = [
-    aws_perimeter.benchmark.public_access,
+    benchmark.aws_iam,
+    benchmark.aws_secrets,
+    aws_perimeter.benchmark.public_access
   ]
 
   tags = merge(local.aws_perimeter_common_tags, {
     type = "Benchmark"
   })
+}
+
+benchmark "aws_iam" {
+  title = "IAM"
+  children = [
+    aws_compliance.control.iam_policy_custom_no_permissive_role_assumption
+  ]
+}
+
+benchmark "aws_secrets" {
+  title = "Exposed Secrets"
+  children = [
+    aws_compliance.control.lambda_function_variables_no_sensitive_data,
+    aws_compliance.control.ec2_instance_user_data_no_secrets,
+    aws_compliance.control.autoscaling_ec2_launch_configuration_no_sensitive_data,
+    aws_compliance.control.ecs_task_definition_container_environment_no_secret,
+    aws_compliance.control.cloudformation_stack_output_no_secrets
+  ]
 }
