2e8bdd367c81f4cac8c27c7149e716bceb83e31a
williamfp4 <william_franz2002@hotmail.com>
2024-06-26 16:15:23 -0300

    Finishing the Security tab + Custom Admin check for IAM Users

--- security.pp
+++ security.pp
@@ -18,15 +18,16 @@ benchmark "aws_iam" {
   title = "IAM"
   children = [
     aws_compliance.control.cis_v300_1_4,
+    aws_compliance.control.cis_v300_1_5,
+    aws_compliance.control.cis_v300_1_7,
     aws_compliance.control.cis_v300_1_10,
-    aws_compliance.control.cis_v120_1_12,
-    aws_compliance.control.cis_v120_1_13,
     aws_compliance.control.cis_v300_1_16,
     aws_compliance.control.cis_v300_1_19,
     aws_compliance.control.foundational_security_iam_1,
     aws_compliance.control.foundational_security_iam_21,
     aws_compliance.control.iam_role_cross_account_read_only_access_policy,
     aws_compliance.control.iam_role_no_administrator_access_policy_attached,
+    control.iam_user_administrator_access,
     aws_compliance.control.ec2_instance_no_iam_role_with_privilege_escalation_risk_access,
     aws_compliance.control.iam_policy_custom_no_permissive_role_assumption
   ]
@@ -36,6 +37,9 @@ benchmark "aws_best_practices"{
   title = "Best practices"
   children = [
     aws_compliance.control.cis_v300_3_1,
+    aws_compliance.control.sns_topic_encrypted_at_rest,
+    aws_compliance.control.acm_certificate_expires_30_days,
+    aws_compliance.control.foundational_security_ssm_2
   ]
 }
 
@@ -44,7 +48,7 @@ benchmark "aws_security_group"{
   children = [
     aws_perimeter.benchmark.security_group_access,
     aws_compliance.control.foundational_security_ec2_19,
-    aws_compliance.control.cis_v200_5_4
+    aws_compliance.control.cis_v300_5_4,
   ]
 }
 
@@ -65,4 +69,42 @@ benchmark "aws_public_access" {
     aws_perimeter.benchmark.public_access,
     aws_compliance.control.emr_account_public_access_blocked
   ]
+}
+
+control "iam_user_administrator_access" {
+  title       = "Ensure IAM Users not attached with AdministratorAccess policy"
+  description = "AWS IAM Users should not be attached AdministratorAccess policy."
+  query       = query.iam_user_administrator_access
+}
+
+query "iam_user_administrator_access" {
+  sql = <<-EOQ
+    with admin_users as (
+        select
+            arn,
+            name,
+            attachments
+        from
+        aws_iam_user,
+        jsonb_array_elements_text(attached_policy_arns) as attachments
+        where
+        split_part(attachments, '/', 2) = 'AdministratorAccess'
+    )
+    select
+        u.arn as resource,
+        case
+            when au.arn is not null then 'alarm'
+        else 'ok'
+        end as status,
+        case
+            when au.arn is not null then u.name || ' have AdministratorAccess policy attached.'
+        else u.name || ' does not have AdministratorAccess policy attached.'
+        end as reason
+    from
+        aws_iam_user as u
+        left join admin_users au on u.arn = au.arn
+    order by
+        u.name;
+    }
+  EOQ
 }
